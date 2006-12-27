4b98fd1eacdd82080e372ec05fc042311a89038a
Rod Begbie <rod@arsecandle.org>
2006-12-27 02:30:32 +0000

    Load posts and links from olde blog

--- threequarters/load.py
+++ threequarters/load.py
@@ -39,7 +39,48 @@ for record in result:
     
     post.created_on = record[5]
     post.modified_on = record[6]
-    post.draft = record[1] == 1
+    post.draft = record[1] <> 2
     post.save()
 
-    post.legacy.create(mt_id=record[0], basename=record[7])
+    legacy = Legacy(mt_id=record[0], basename=record[7])
+    legacy.save()
+    
+    blogitem = post.blogitem.get()
+    blogitem.legacy = legacy
+    blogitem.save()
+
+cursor.execute("""SELECT entry_id, 
+                    entry_excerpt,
+                    entry_title, 
+                    entry_text, 
+                    entry_text_more,
+                    entry_created_on,
+                    entry_modified_on,
+                    entry_basename
+                  FROM mt_entry
+                  WHERE entry_blog_id = 6""")
+# get the resultset as a tuple
+result = cursor.fetchall()
+# iterate through resultset
+for record in result:
+    print record[0] , "-->", record[2]
+    post = Link()
+    post.title = record[2]
+    post.description = record[3]
+    post.url = record[4]
+
+    via = ""
+    if record[1]:
+        via = record[1]
+    post.via = via
+
+    post.created_on = record[5]
+    post.modified_on = record[6]
+    post.save()
+
+    legacy = Legacy(mt_id=record[0], basename=record[7])
+    legacy.save()
+    
+    blogitem = post.blogitem.get()
+    blogitem.legacy = legacy
+    blogitem.save()
