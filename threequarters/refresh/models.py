aa86f8021e1f1678cc2ce5bc0ae96d36ca43dcfe
Rod Begbie <rod@arsecandle.org>
2007-09-02 00:52:11 +0000

    auto_now_add

--- threequarters/refresh/models.py
+++ threequarters/refresh/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from datetime import datetime
 
 class Weight(models.Model):
     """(Weight description)"""
     weight = models.FloatField(max_digits=5, decimal_places=1)
     underwear = models.BooleanField(default=True)
     jeans = models.BooleanField(default=False)
-    time = models.DateTimeField(auto_add_now=True)
+    time = models.DateTimeField(auto_now_add=True)
 
     class Admin:
         list_display = ('time', 'weight')
@@ -18,7 +17,7 @@ class ShangriLaOil(models.Model):
     """(ShangriLaOil description)"""
     
     tablespoons = models.FloatField(max_digits=3, decimal_places=1)
-    time = models.DateTimeField(auto_add_now=True)
+    time = models.DateTimeField(auto_now_add=True)
 
     class Admin:
         list_display = ('time','tablespoons')
