8095feb7821d567c3fc11ccb5b654ddb76ef6040
Rod Begbie <rod@arsecandle.org>
2008-03-05 23:45:21 +0000

    Added fireeagle getter

---
+++ threequarters/fireeagle/fetchFireEagle.py
@@ -0,0 +1,28 @@
+CONSUMER_SECRET='gQYRSEgCsEZSCDM2Pa6qr9aTVCmT80Mx'
+CONSUMER_KEY='3q3VkoxUjwP9'
+
+USER_TOKEN_STRING='oauth_token_secret=8gMH42725VgaLB4b3iahYLdxozVawLlM&oauth_token=VhfSR2SzLkzM'
+
+from fireeagle_api import FireEagle
+from oauth import OAuthToken
+
+from threequarters.blog.models import Location
+import datetime
+
+fe = FireEagle(CONSUMER_KEY, CONSUMER_SECRET)
+token = OAuthToken.from_string(USER_TOKEN_STRING)
+
+locations = fe.user(token)
+#from pprint import pprint
+#pprint(locations)
+
+lastloc = Location.objects.all()[0]
+for location in locations:
+    if location["level"] == 3: # CITY
+        name = location["name"]
+	located_at = location["located_at"] + datetime.timedelta(hours=3)
+	#print "LOCATED_AT", located_at, "LAST", lastloc.located_at
+	if located_at <> lastloc.located_at:
+	    print "ADDING LOCATION", name, loc
+            loc = Location(name=name, located_at=located_at)
+	    loc.save()
