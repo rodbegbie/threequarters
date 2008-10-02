8c29a32ba5781da107e816c3b50485e982d45faf
Rod Begbie <rod@arsecandle.org>
2008-10-02 04:20:22 +0000

    Don't show modified_on on admin

--- threequarters/blog/admin.py
+++ threequarters/blog/admin.py
@@ -5,14 +5,14 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'created_on', 'draft')
     list_filter = ['created_on', 'draft']
     prepopulated_fields = {"slug": ("title",)}
-    exclude = ('modified_on',)
+    exclude = ['modified_on']
 admin.site.register(Post, PostAdmin)
 
 class LinkAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_on')
     list_filter = ['created_on']
     prepopulated_fields = {"slug": ("title",)}
-    exclude = ('modified_on',)
+    exclude = ['modified_on']
 admin.site.register(Link, LinkAdmin)
 
 class LocationAdmin(admin.ModelAdmin):
