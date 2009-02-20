a553e904c04a106f9e2d44311dd0cdcd5a2e2654
Rod Begbie <rod@arsecandle.org>
2009-02-20 05:58:05 +0000

    Get more tweets, adjust for winter time

--- threequarters/jobs/loadTwitter.py
+++ threequarters/jobs/loadTwitter.py
@@ -3,7 +3,7 @@ from urllib import urlopen
 import time, datetime
 from threequarters.blog.models import Twitter
 
-url = 'http://twitter.com/statuses/user_timeline/rodbegbie.json?count=5'
+url = 'http://twitter.com/statuses/user_timeline/rodbegbie.json?count=20'
 json = urlopen(url).read()
 
 for entry in simplejson.loads(json):
@@ -13,6 +13,6 @@ for entry in simplejson.loads(json):
     print desc
     (twitter, new) = Twitter.objects.get_or_create(twitter_id = entry["id"])
     twitter.description = desc
-    twitter.created_on = datetime.datetime(*(time.strptime(entry["created_at"], "%a %b %d %H:%M:%S +0000 %Y")[0:6])) - datetime.timedelta(hours=7)
+    twitter.created_on = datetime.datetime(*(time.strptime(entry["created_at"], "%a %b %d %H:%M:%S +0000 %Y")[0:6])) - datetime.timedelta(hours=8)
     twitter.save()
 
